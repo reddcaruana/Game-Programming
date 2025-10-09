f11a540742b436f6203dbc6833f3f966db44518a
Redd <35120080+reddcaruana@users.noreply.github.com>
2025-10-09 13:57:27 +0200

    feat: Basic Variables

---
+++ Assets/2_ProgFun/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+// Defines a library in C#
+namespace _2_ProgFun
+{
+    public class PlayerController : MonoBehaviour
+    {
+        // The four simple data types that are used in games
+        [SerializeField] // Exposes private variables inside the Inspector
+        [Min(0)]
+        private int health = 100; // an integer - a whole number
+        
+        [SerializeField]
+        [Range(1f, 5f)]
+        private float speed = 5f; // a float - a number with a point, followed by f
+        
+        [SerializeField]
+        private string playerName = "Hero"; // a string - collection of characters
+        
+        [SerializeField]
+        private bool isAlive = true; // a boolean - always true/false
+    }
+}
