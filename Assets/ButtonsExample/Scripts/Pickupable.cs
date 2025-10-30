091c3cf3eff491c993839083f4f7d401bca55e7c
Redd <35120080+reddcaruana@users.noreply.github.com>
2025-10-30 13:57:51 +0100

    feat: Carrying and Dropping an item

--- Assets/ButtonsExample/Scripts/Pickupable.cs
+++ Assets/ButtonsExample/Scripts/Pickupable.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 namespace ButtonsExample
 {
+    /// <summary>
+    /// This script handles the act of picking up and dropping an object.
+    /// </summary>
     [RequireComponent(typeof(Rigidbody))]
     public class Pickupable : MonoBehaviour
     {
@@ -12,14 +15,36 @@ namespace ButtonsExample
             myRigidbody = GetComponent<Rigidbody>();
         }
 
-        public void Pickup()
+        private void FixedUpdate()
+        {
+            // Early return if the object isn't picked up
+            if (transform.parent == null) return;
+            var target = transform.parent;
+            
+            // Moves the object so that it always matches the parent
+            myRigidbody.MovePosition(target.position); // Moves in place
+            myRigidbody.MoveRotation(target.rotation); // Rotates to face the player
+        }
+
+        public void Pickup(Transform target)
         {
             Debug.Log("I was picked up");
+            
+            // Parent this to the "hand" (currently)
+            transform.SetParent(target);
+
+            // Problem: If we don't move this manually, it hovers in place
+            myRigidbody.isKinematic = true; // Deactivates physics (it will stick to the hand)
         }
 
         public void Drop()
         {
             Debug.Log("I was dropped");
+            
+            // Unparent from the hand
+            transform.SetParent(null);
+
+            myRigidbody.isKinematic = false; // Activates physics
         }
     }
 }
--- Assets/ButtonsExample/Scripts/PlayerCarry.cs
+++ Assets/ButtonsExample/Scripts/PlayerCarry.cs
@@ -8,20 +8,48 @@ namespace ButtonsExample
     {
         [SerializeField] private Transform source; // Where I'm checking for items
         [SerializeField] private float radius = 1f; // How big the range is
+
+        // The object that we're currently holding
+        [SerializeField]
+        private Pickupable objectInHands;
         
         private void OnInteract(InputValue value)
         {
+            // If we're already holding an object, there's no need to continue the method
+            if (objectInHands != null)
+            {
+                objectInHands.Drop();
+                objectInHands = null;
+                return;
+            }
+            
             // The origin point
             var origin = source.position;
-            var results = new RaycastHit[4]; // The result of our raycast
+            var results = new Collider[4]; // The result of our raycast
             
             // Will return the number of objects found in space
-            var hitCount = Physics.SphereCastNonAlloc(origin, radius, transform.forward, results);
+            var hitCount = Physics.OverlapSphereNonAlloc(origin, radius, results);
 
             // Quick loop to print object names
             for (var i = 0; i < hitCount; i++)
             {
-                Debug.Log($"I found {results[i].transform.name}");
+                // Make a result readable
+                var result = results[i];
+                if (result.attachedRigidbody == null) continue; // Breaks the loop
+
+                // Look for the attached Rigidbody
+                // Try to get the Pickupable script
+                // If it doesn't exist, this loop fails
+                // ! equates to a false
+                if (!result.attachedRigidbody.TryGetComponent<Pickupable>(out var pickupable))
+                    continue;
+
+                // Pick up the object
+                objectInHands = pickupable; // Lets the script know we're holding an object
+                pickupable.Pickup(source);
+                
+                // Breaks the function if one item was picked up
+                return;
             }
         }
     }
